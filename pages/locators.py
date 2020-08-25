73e2ec45ffeb844046ef19186cc1c5b541074e30
Stepan Churakov <churakov.s@gmail.com>
2020-08-25 20:36:42 +0300

    add tests for login page

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,13 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    PAGE_URL = ""
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_FORM_EMAIL = (By.CSS_SELECTOR, "[name='registration-email']")
+    REGISTRATION_FORM_PASS = (By.CSS_SELECTOR, "[name='registration-password1']")
+    REGISTRATION_FORM_CONFIRM_PASS = (By.CSS_SELECTOR, "[name='registration-password2']")
+    LOGIN_FORM_EMAIL = (By.CSS_SELECTOR, "[name='login-username']")
+    LOGIN_FORM_PASS = (By.CSS_SELECTOR, "[name='login-password']")
